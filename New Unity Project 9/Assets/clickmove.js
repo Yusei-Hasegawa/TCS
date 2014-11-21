c827f7a13cb48d2c2c79ff5c95eb63890458bfea
TaishiSada <b1012235@gmail.com>
2014-11-21 17:14:32 +0900

    LoLしたい

---
+++ New Unity Project 9/Assets/clickmove.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var smooth:int; // Determines how quickly object moves towards position
+private var targetPosition:Vector3;
+var speed = 5;
+ 
+function Update () {
+ 
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+                smooth=1;
+                var playerPlane = new Plane(Vector3.up, transform.position);
+                var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+                var hitdist = 0.0;
+ 
+                if (playerPlane.Raycast (ray, hitdist)) {
+ 
+                        var targetPoint = ray.GetPoint(hitdist);
+                        targetPosition = ray.GetPoint(hitdist);
+                        var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                        transform.rotation = targetRotation;
+                }
+        }
+ 
+        var dir:Vector3 = targetPosition - transform.position;
+        var dist:float = dir.magnitude;
+        var move:float = speed * Time.deltaTime;
+ 
+        if(dist > move){
+                transform.position += dir.normalized * move;
+ 
+        }
+        else {
+                transform.position = targetPosition;
+        }
+ 
+        transform.position += (targetPosition - transform.position).normalized * speed * Time.deltaTime;
+ 
+}
